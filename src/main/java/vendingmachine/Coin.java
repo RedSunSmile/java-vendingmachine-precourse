609c7ff52ecd3d281d34697e7a0b8e033b282780
RedSunSmile <111857426+RedSunSmile@users.noreply.github.com>
2026-09-01 21:31:16 +0900

    feat: 자판기 보유 동전 무작위 생성

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -13,4 +13,17 @@ public enum Coin {
     }
 
     // 추가 기능 구현
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public static Coin from(int amount) {
+        for (Coin coin : values()) {
+            if (coin.amount == amount) {
+                return coin;
+            }
+        }
+        throw new IllegalArgumentException("[ERROR] 존재하지 않는 동전입니다.");
+    }
 }
---
+++ src/main/java/vendingmachine/service/CoinGenerator.java
@@ -0,0 +1,35 @@
+package vendingmachine.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import vendingmachine.Coin;
+
+import java.util.*;
+
+public class CoinGenerator {
+
+    public Map<Coin, Integer> generate(int totalAmount) {
+        Map<Coin, Integer> result = new EnumMap<>(Coin.class);
+        for (Coin coin : Coin.values()) {
+            result.put(coin, 0);
+        }
+        int remain = totalAmount;
+
+        calculateAboutCoins(remain, result);
+
+        return result;
+    }
+
+    private static void calculateAboutCoins(int remain, Map<Coin, Integer> result) {
+        while (remain > 0) {
+            List<Integer> candidates = new ArrayList<>();
+            for (Coin coin : Coin.values()) {
+                if (coin.getAmount() <= remain) {
+                    candidates.add(coin.getAmount());
+                }
+            }
+            int picked = Randoms.pickNumberInList(candidates);
+            result.put(Coin.from(picked), result.get(Coin.from(picked))+1);
+            remain-=picked;
+        }
+    }
+}
