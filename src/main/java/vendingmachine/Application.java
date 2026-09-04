d709a10f0a9350a38aca55481ef1c94e5e706e88
RedSunSmile <111857426+RedSunSmile@users.noreply.github.com>
2026-09-04 22:45:25 +0900

    feat: 상품 입력을 Products로 연결

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,18 +1,26 @@
 package vendingmachine;
 
 import vendingmachine.domain.Coins;
+import vendingmachine.domain.Product;
+import vendingmachine.domain.Products;
 import vendingmachine.service.CoinGenerator;
 import vendingmachine.ui.InputView;
 import vendingmachine.ui.OutputView;
+
+import java.util.List;
 import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
+
         CoinGenerator coinGenerator = new CoinGenerator();
-        Map<Coin,Integer> coins=coinGenerator.generate(inputView.inputOfVendingMachine());
+        Map<Coin, Integer> coins = coinGenerator.generate(inputView.inputOfVendingMachine());
         OutputView outputView = new OutputView();
-        Coins coines=new Coins(coins);
-        outputView.coinsInVendingMachine(coines);
+        Coins sources = new Coins(coins);
+        outputView.coinsInVendingMachine(sources);
+        String productInput= inputView.inputOfProduct();
+        Products products=new Products(productInput);
+
     }
 }
