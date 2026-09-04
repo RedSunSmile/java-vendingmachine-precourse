1bf803b9eebe602908edfbb82c7dcd8dccf62827
RedSunSmile <111857426+RedSunSmile@users.noreply.github.com>
2026-09-04 20:18:17 +0900

    refactor: Coins 생성자 매개변수 이름을 source로 변경

--- src/main/java/vendingmachine/domain/Coins.java
+++ src/main/java/vendingmachine/domain/Coins.java
@@ -8,11 +8,10 @@ import java.util.Map;
 public class Coins {
     private final Map<Coin, Integer> coins;
 
-    public Coins(Map<Coin, Integer> coins) {
+    public Coins(Map<Coin, Integer> source) {
         Map<Coin, Integer> copy = new EnumMap<>(Coin.class);
-
         for (Coin coin : Coin.values()) {
-            copy.put(coin, coins.getOrDefault(coin, 0));
+            copy.put(coin, source.getOrDefault(coin, 0));
         }
         this.coins = copy;
     }
