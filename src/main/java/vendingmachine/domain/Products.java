33bfbc11de86eedbfb509a69710f970bd698ffe9
RedSunSmile <111857426+RedSunSmile@users.noreply.github.com>
2026-09-07 21:16:47 +0900

    feat: 상품명 조회 및 최저 가격 계산 추가

--- src/main/java/vendingmachine/domain/Products.java
+++ src/main/java/vendingmachine/domain/Products.java
@@ -4,29 +4,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Products {
-  private final List<Product> products;
+    private final List<Product> products;
 
     public Products(String input) {
         this.products = parse(input);
     }
 
     private Product splitProducts(String input) {
-        String result=input.replace("[","");
-        String result2=result.replace("]","");
-        String[] parts =result2.split(",");
-        String name =parts[0];
-        int price=Integer.parseInt(parts[1]);
-        int counts=Integer.parseInt(parts[2]);
-       return new Product(name,price,counts);
+        String result = input.replace("[", "");
+        String result2 = result.replace("]", "");
+        String[] parts = result2.split(",");
+        String name = parts[0];
+        int price = Integer.parseInt(parts[1]);
+        int counts = Integer.parseInt(parts[2]);
+        return new Product(name, price, counts);
     }
 
-    private List<Product> parse(String input){
-        String[] chunks=input.split(";");
-        List<Product> products=new ArrayList<>();
-        for(String chunk:chunks){
+    private List<Product> parse(String input) {
+        String[] chunks = input.split(";");
+        List<Product> products = new ArrayList<>();
+        for (String chunk : chunks) {
             products.add(splitProducts(chunk));
         }
         return products;
     }
 
+    public Product findByName(String name) {
+        for (Product product : products) {
+            if (product.takeItem().equals(name))
+                return product;
+        }
+        throw new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다.");
+    }
+
+
+    public boolean isAllSoldOut() {
+        for (Product product : products) {
+            if (!product.isSoldOut()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int minPrice() {
+        int min = Integer.MAX_VALUE;
+        for (Product product : products) {
+            if (product.takePrice() < min)
+                min = product.takePrice();
+        }
+        return min;
+
+    }
 }
