cf86d356a13785e23e840dfb79400aa6adada43f
RedSunSmile <111857426+RedSunSmile@users.noreply.github.com>
2026-09-10 22:50:46 +0900

    feat: 최소 개수 동전으로 잔돈 계산 추가

--- src/main/java/vendingmachine/domain/Coins.java
+++ src/main/java/vendingmachine/domain/Coins.java
@@ -2,7 +2,9 @@ package vendingmachine.domain;
 
 import vendingmachine.Coin;
 
+import java.util.ArrayList;
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
 
 public class Coins {
@@ -19,4 +21,33 @@ public class Coins {
     public int countOf(Coin coin) {
         return coins.get(coin);
     }
+
+    public Coins change(int amount) {
+        Map<Coin, Integer> returned = new EnumMap<>(Coin.class);
+        while (amount > 0) {
+            List<Integer> changeMoney = availableCoins(amount);
+            if (changeMoney.isEmpty()) {
+                break;
+            }
+            int picked = changeMoney.get(0);
+            amount -= picked;
+            Coin pickedCoin = Coin.from(picked);
+            coins.put(pickedCoin, countOf(pickedCoin) - 1);//동전개수줄이기
+            returned.put(pickedCoin, returned.getOrDefault(pickedCoin, 0) + 1);
+        }
+        return new Coins(returned);
+    }
+
+    //자판기 보유잔돈 개수세
+    private List<Integer> availableCoins(int amount) {
+        List<Integer> changeMoney = new ArrayList<>();
+        for (Coin coin : Coin.values()) {
+            if (coin.getAmount() <= amount && countOf(coin) > 0) {
+                changeMoney.add(coin.getAmount());
+            }
+        }
+        return changeMoney;
+    }
+
+
 }
