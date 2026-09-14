00675aa3c51ff6f89703a11ed67fffea6cfb728c
RedSunSmile <111857426+RedSunSmile@users.noreply.github.com>
2026-09-14 22:30:23 +0900

    refactor: Products 반복문을 stream과 Optional로 변경

--- src/main/java/vendingmachine/domain/Products.java
+++ src/main/java/vendingmachine/domain/Products.java
@@ -1,7 +1,9 @@
 package vendingmachine.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Products {
     private final List<Product> products;
@@ -11,53 +13,38 @@ public class Products {
     }
 
     private Product splitProducts(String input) {
-        String result = input.replace("[", "");
-        String result2 = result.replace("]", "");
-        String[] parts = result2.split(",");
+       return Optional.of(input.replace("[","")
+               .replace("]","")
+                .split(","))
+               .filter(parts->parts.length==3)
+               .map(parts->new Product(parts[0],Integer.parseInt(parts[1]),Integer.parseInt(parts[2])))
+               .orElseThrow(()->new IllegalArgumentException("[ERROR] 상품 형식이 올바르지 않습니다."));
 
-        if (parts.length != 3) {
-            throw new IllegalArgumentException("[ERROR] 상품 형식이 올바르지 않습니다.");
-        }
-        String name = parts[0];
-        int price = Integer.parseInt(parts[1]);
-        int counts = Integer.parseInt(parts[2]);
-        return new Product(name, price, counts);
     }
 
     private List<Product> parse(String input) {
-        String[] chunks = input.split(";");
-        List<Product> products = new ArrayList<>();
-        for (String chunk : chunks) {
-            products.add(splitProducts(chunk));
-        }
-        return products;
+        return Arrays.stream(input.split(";"))
+                .map(chunk->splitProducts(chunk))
+                .toList();
     }
 
     public Product findByName(String name) {
-        for (Product product : products) {
-            if (product.takeItem().equals(name))
-                return product;
-        }
-        throw new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다.");
+       return products.stream()
+               .filter(product -> product.takeItem().equals(name))
+               .findAny()
+               .orElseThrow(()->new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다."));
     }
 
-
     public boolean isAllSoldOut() {
-        for (Product product : products) {
-            if (!product.isSoldOut()) {
-                return false;
-            }
-        }
-        return true;
+        return products.stream()
+                .allMatch(Product::isSoldOut);
     }
 
     public int minPrice() {
-        int min = Integer.MAX_VALUE;
-        for (Product product : products) {
-            if (!product.isSoldOut() && product.takePrice() < min) //품절제외 & 음료금액 부족할 때 =>최저가고르기
-                min = product.takePrice();
-        }
-        return min;
-
+        return products.stream()
+                .filter(product -> !product.isSoldOut())
+                .mapToInt(Product::takePrice)
+                .min()
+                .orElse(Integer.MAX_VALUE);
     }
 }
