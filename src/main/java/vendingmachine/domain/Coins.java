b396a4c28cbabc27bbbb26c505c15a64fc7b1823
RedSunSmile <111857426+RedSunSmile@users.noreply.github.com>
2026-09-16 17:49:37 +0900

    refactor: 잔돈 계산 파라미터명을 remain으로 변경

--- src/main/java/vendingmachine/domain/Coins.java
+++ src/main/java/vendingmachine/domain/Coins.java
@@ -17,15 +17,15 @@ public class Coins {
         return coins.get(coin);
     }
 
-    public Coins change(int amount) {
+    public Coins change(int remain) {
         Map<Coin, Integer> returned = new EnumMap<>(Coin.class);
-        while (amount > 0) {
-            List<Integer> changeMoney = availableCoins(amount);
+        while (remain > 0) {
+            List<Integer> changeMoney = availableCoins(remain);
             if (changeMoney.isEmpty()) {
                 break;
             }
             int picked = changeMoney.get(0);
-            amount -= picked;
+            remain -= picked;
             Coin pickedCoin = Coin.from(picked);
             coins.put(pickedCoin, countOf(pickedCoin) - 1);//동전개수줄이기
             returned.put(pickedCoin, returned.getOrDefault(pickedCoin, 0) + 1);
@@ -34,12 +34,10 @@ public class Coins {
     }
 
     //자판기 보유잔돈 개수세기
-    private List<Integer> availableCoins(int amount) {
+    private List<Integer> availableCoins(int remain) {
         return Arrays.stream(Coin.values())
-                .filter(coin -> coin.takeAmount() <= amount && countOf(coin) > 0)
+                .filter(coin -> coin.takeAmount() <= remain && countOf(coin) > 0)
                 .map(coin -> coin.takeAmount())
                 .toList();
-
     }
-
 }
